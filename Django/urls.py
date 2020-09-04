ce30fdc29b40498abb59557b2e29904a551ac014
pyaephyohein <pyaephyohein.info.3326@gmail.com>
2020-09-04 09:48:09 +0630

    add models

--- Django/urls.py
+++ Django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import index
 
 urlpatterns = [
+    path('todo/', index),
     path('admin/', admin.site.urls),
 ]
